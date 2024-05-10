4e0f7cad1acc7c150b2ceedb067e675d45c8d568
CoolRadish <coool.radish.x@gmail.com>
2024-05-11 03:42:24 +0800

    Update code

--- algorithm/plus-minus/solutions/plusMinus.js
+++ algorithm/plus-minus/solutions/plusMinus.js
@@ -2,16 +2,7 @@
  * Time complexity - O(|arr|)
  */
 function plusMinus(arr) {
-  let plus = 0;
-  let minus = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) plus++;
-    if (arr[i] < 0) minus++;
-  }
-
-  console.log((plus / arr.length).toFixed(6));
-  console.log((minus / arr.length).toFixed(6));
-  console.log(
-    1 - (plus / arr.length).toFixed(6) - (minus / arr.length).toFixed(6)
-  );
+  arr
+    .reduce((acc, v) => (acc[(Math.sign(v) + 2) % 3]++, acc), [0, 0, 0])
+    .forEach((count) => console.log((count / arr.length).toFixed(6)));
 }
